1e91a3b939a61d84f33ef7625e2b522587df5849
Daniel Andrés Triana Camacho <47016495+dantrica@users.noreply.github.com>
2025-08-14 18:20:22 +0200

    Update read_voltage.py

--- examples/read_voltage.py
+++ examples/read_voltage.py
@@ -1,20 +1,40 @@
+"""
+# Include the code lines below if you are running directly on Jupyter from your Raspberry Pi 
+import sys
+sys.path.append('address to ads1256.py and constants.py')
+"""
 import time
+import matplotlib.pyplot as plt
+import pandas as pd
+from ads1256 import ADS1256
+from constants import ADS1256Constants as ADSC
+import RPi.GPIO as GPIO
 
-from ads1256.ads1256 import ADS1256
-from ads1256.constants import ADS1256Constants as ADSC  # noqa: N817
+# ---------------------------- Configuration ----------------------------
 
-if __name__ == '__main__':
-    ads = ADS1256()
-    ads.set_input(ADSC.POS_AIN0, ADSC.NEG_AINCOM)
-    ads.set_gain(1)
+# Initialize ADS1256 instance with custom SPI and GPIO settings
+adc = ADS1256(
+    spi_bus=0,
+    spi_device=1,          # Using SPI0.1 to avoid conflict with manual CS
+    spi_frequency=100000,  # Recommended 50 kHz SPI clock, others 10 kHz, 100 kHz, not recommended 500 kHz, 1000 kHz and the maximum 2 MHz.
+    cs_pin=8,              # Chip select manually controlled on GPIO8
+    data_ready_pin=22,     # DRDY pin on GPIO22
+    v_ref=2.6518           # Reference voltage in volts
+)
 
-    while True:
-        value = ads.read_value()
-        voltage = value * ads.volt_per_digit()
+# Enable Buffer. True Recommended for signals with high impedance (Resistors, Sensors), on the contrary False for signals with low impedance (Power Supplies)
+adc.enable_buffer(True)
 
-        # You can also directly read the voltage.
-        # voltage = ads.read_voltage()
+# Set gain (amplification) and sample rate
+adc.set_gain(1)  # Gain options: 1,2,4,8,16,32,64
+adc.set_data_rate(ADSC.DRATE_1000)  # Choose sample rate (1000 SPS here)
 
-        print(f'{value}\t\t{voltage:.5f} V')
+# ----------------------- Single Channel Read Example -------------------
 
-        time.sleep(0.01)
+# Set input channel to AIN0 vs AINCOM
+adc.set_input(ADSC.POS_AIN0, ADSC.NEG_AINCOM)
+#adc.set_input(ADSC.POS_AIN1, ADSC.NEG_AIN0)
+
+# Read voltage from AIN0
+voltage_ain0 = adc.read_voltage()
+print(f"AIN0 Voltage: {voltage_ain0:.6f} V")
